b3a0603190edcdc58721d7b49b8f0c141fe92de6
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-03-19 16:04:36 +0200

    3-effects-and-data-fetching add data-fetching loading and error state carousel

--- src/pages/HomePage.jsx
+++ src/pages/HomePage.jsx
@@ -1,40 +1,70 @@
-import { useState } from 'react';
+import axios from 'axios';
+import { useEffect, useRef, useState } from 'react';
 
-import {
-  isListingAvailable,
-  listings as staticListings,
-} from '@/api/data/listings';
+import api from '@/api';
 import ListingFilters from '@/components/ListingFilters';
 import ListingList from '@/components/ListingList';
-import { Separator } from '@/components/ui';
+import { Separator, Spinner } from '@/components/ui';
 
 const HomePage = () => {
-  const [listings, setListings] = useState(staticListings);
+  const [listings, setListings] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [error, setError] = useState(null);
+  const [filters, setFilters] = useState({
+    dates: undefined,
+    guests: 0,
+    search: '',
+  });
+
+  const abortController = useRef(null);
+
+  useEffect(() => {
+    const fetchListings = async () => {
+      setIsLoading(true);
+      setError(null);
+
+      abortController.current = new AbortController();
+
+      try {
+        const response = await api.get('/api/listings', {
+          params: filters,
+          signal: abortController.current?.signal,
+        });
+        setListings(response.data);
+      } catch (error) {
+        if (axios.isCancel(error)) {
+          return;
+        }
+        setError('Something went wrong. Please try again later.');
+      } finally {
+        setIsLoading(false);
+      }
+    };
+
+    fetchListings();
+
+    return () => {
+      abortController.current?.abort();
+    };
+  }, [filters]);
 
   const handleFilters = (filters) => {
-    const { dates, guests, search } = filters;
+    setFilters(filters);
+  };
 
-    let filteredListings = staticListings;
-
-    if (dates) {
-      filteredListings = filteredListings.filter((listing) =>
-        isListingAvailable(listing, dates),
+  const renderListingList = () => {
+    if (isLoading) {
+      return (
+        <div className='flex justify-center'>
+          <Spinner size='sm' />
+        </div>
       );
     }
 
-    if (guests) {
-      filteredListings = filteredListings.filter(
-        (listing) => guests <= listing.maxGuests,
-      );
+    if (error) {
+      return <div className='text-center'>{error}</div>;
     }
-
-    if (search) {
-      filteredListings = filteredListings.filter((listing) =>
-        listing.name.toLowerCase().includes(search.toLowerCase()),
-      );
-    }
-
-    setListings(filteredListings);
+    return <ListingList listings={listings} />;
   };
 
   return (
@@ -43,7 +73,7 @@ const HomePage = () => {
         <ListingFilters onChange={handleFilters} />
         <Separator className='my-4' />
       </div>
-      <ListingList listings={listings} />
+      {renderListingList()}
     </div>
   );
 };
