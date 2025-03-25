dcd1c424f6f2c42ae2b5aa69a3b6a780d7171464
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-03-25 18:44:59 +0200

    7-forms-and-authentication add auth protect sign out btn

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,19 @@
 import { Outlet } from 'react-router-dom';
 
+import { useAuth } from '@/components/AuthProvider';
 import Devbar from '@/components/Devbar/Devbar';
-import NavBar from '@/components/NavBar';
+import Navbar from '@/components/Navbar';
 
 const App = () => {
+  const { token } = useAuth();
+
   return (
     <>
       <div className='fixed bottom-0 left-0 top-0'>
         <Devbar />
       </div>
       <div className='ml-[700px]'>
-        <NavBar />
+        {token && <Navbar />}
         <Outlet />
       </div>
     </>
--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,17 +1,50 @@
 import { Link } from 'react-router-dom';
 
-import { Separator } from '@/components/ui';
+import api from '@/api';
+import { useAuth } from '@/components/AuthProvider';
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+  Separator,
+} from '@/components/ui';
 
-const NavBar = () => {
+const Navbar = () => {
+  const { setToken } = useAuth();
+
+  const handleSignOut = async () => {
+    try {
+      await api.post('/api/signout');
+
+      setToken(null);
+    } catch {
+      setToken(null);
+    }
+  };
+
   return (
     <>
-      <div className='flex flex-row justify-center gap-8 px-8 py-4'>
+      <div className='flex flex-row items-center justify-between gap-8 px-8 py-4'>
         <Link to='/'>Home</Link>
-        <Link to='/favorites'>Favorites</Link>
+        <div className='flex-end flex flex-row items-center gap-8'>
+          <Link to='/favorites'>Favorites</Link>
+
+          <DropdownMenu>
+            <DropdownMenuTrigger asChild>
+              <Link>Account</Link>
+            </DropdownMenuTrigger>
+            <DropdownMenuContent align='end'>
+              <DropdownMenuItem onClick={handleSignOut}>
+                Sign Out
+              </DropdownMenuItem>
+            </DropdownMenuContent>
+          </DropdownMenu>
+        </div>
       </div>
       <Separator />
     </>
   );
 };
 
-export default NavBar;
+export default Navbar;
