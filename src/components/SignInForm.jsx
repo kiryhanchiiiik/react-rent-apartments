e1581b3e5a681b0967d255a5e960763475672fae
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-03-26 18:32:48 +0200

    improved form and inputs

--- src/components/SignInForm.jsx
+++ src/components/SignInForm.jsx
@@ -9,9 +9,11 @@ import {
   Card,
   CardContent,
   CardHeader,
-  Input,
   Separator,
 } from '@/components/ui';
+
+import TextInput from '@/components/TextInput';
+import Form from '@/components/Form';
 
 const signInFormSchema = z.object({
   email: z.string().email(),
@@ -21,12 +23,7 @@ const signInFormSchema = z.object({
 const SignInForm = () => {
   const { setToken } = useAuth();
 
-  const {
-    formState: { errors, isSubmitting },
-    handleSubmit,
-    register,
-    setError,
-  } = useForm({
+  const form = useForm({
     resolver: zodResolver(signInFormSchema),
   });
 
@@ -35,7 +32,7 @@ const SignInForm = () => {
       const response = await api.post('/api/signin', data);
       setToken(response.data.accessToken);
     } catch (e) {
-      setError('root', {
+      form.setError('root', {
         message: e.response.data.message,
       });
     }
@@ -51,35 +48,20 @@ const SignInForm = () => {
         <Separator />
       </CardHeader>
       <CardContent>
-        <form className='flex flex-col gap-4'>
-          <div>
-            <Input {...register('email')} placeholder='name@example.com' />
-            {errors['email'] && (
-              <div className='mt-2 text-sm text-red-500'>
-                {errors['email'].message}
-              </div>
-            )}
-          </div>
-
-          <div>
-            <Input {...register('password')} type='password' />
-            {errors['password'] && (
-              <div className='mt-2 text-sm text-red-500'>
-                {errors['password'].message}
-              </div>
-            )}
-          </div>
-
-          <Button disabled={isSubmitting} onClick={handleSubmit(onSubmit)}>
-            {isSubmitting ? 'Loading...' : 'Sign In'}
+        <Form form={form}>
+          <TextInput
+            control={form.control}
+            name='email'
+            placeholder='name@example.com'
+          />
+          <TextInput control={form.control} name='password' type='password' />
+          <Button
+            disabled={form.formState.isSubmitting}
+            onClick={form.handleSubmit(onSubmit)}
+          >
+            {form.formState.isSubmitting ? 'Loading...' : 'Sign In'}
           </Button>
-
-          {errors.root && (
-            <div className='text-center text-sm text-red-500'>
-              {errors.root.message}
-            </div>
-          )}
-        </form>
+        </Form>
       </CardContent>
     </Card>
   );
---
+++ src/components/TextInput.jsx
@@ -0,0 +1,26 @@
+import { useController } from 'react-hook-form';
+
+import { Input } from '@/components/ui';
+
+const TextInput = ({ control, name, type = 'text', ...rest }) => {
+  const form = useController({ control, name });
+
+  const error = form.formState.errors[name];
+
+  return (
+    <div>
+      <Input
+        {...rest}
+        type={type}
+        onChange={form.field.onChange}
+        onBlur={form.field.onBlur}
+        value={form.field.value || ''}
+      />
+      {error && (
+        <div className='mt-2 text-sm text-red-500'>{error.message}</div>
+      )}
+    </div>
+  );
+};
+
+export default TextInput;
