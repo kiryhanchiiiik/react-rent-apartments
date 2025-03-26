0934f9c4adbc867dc4c03a2380db03aed3084891
kiryhanchiiiik <162802442+kiryhanchiiiik@users.noreply.github.com>
2025-03-26 17:55:25 +0200

    fix navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,7 +2,7 @@ import { Outlet } from 'react-router-dom';
 
 import { useAuth } from '@/components/AuthProvider';
 import Devbar from '@/components/Devbar/Devbar';
-import Navbar from '@/components/Navbar';
+import Navbar from '@/components/Navbar.jsx';
 
 const App = () => {
   const { token } = useAuth();
